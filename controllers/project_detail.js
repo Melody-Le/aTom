92a7ef152f9ba6bb2c5e0e7aad76c4a735809f07
MUI LE <tanmui1992@gmail.com>
2022-07-31 12:30:28 +0800

    feat: add project photo to project detail

--- controllers/project_detail.js
+++ controllers/project_detail.js
@@ -2,11 +2,12 @@ const ProjectDetail = require("../models/project_detail.js");
 const ProjectPhotos = require("../models/project_photo.js");
 const controller = {
   async showProjectDetailPage(req, res) {
-    // const projectPhotos = await ProjectPhotos.find({}, "photo_url");
+    const projectPhotos = await ProjectPhotos.find({ project_detail_id: "62e5ee63767fa1299ea95b18" }, "photo_url");
     // res.render("./idea-markets/index.ejs", {
     //   projectPhotos,
     // });
-    res.render("./project-detail/index.ejs");
+    console.log(projectPhotos);
+    res.render("./project-detail/index.ejs", { projectPhotos });
   },
 };
 module.exports = controller;
