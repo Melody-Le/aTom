65d284a46cf137f12d2ee284436b400c24ef3485
MUI LE <tanmui1992@gmail.com>
2022-07-31 14:40:16 +0800

    fix: navigate idea-market with project detail by project_detail_id

--- controllers/project_detail.js
+++ controllers/project_detail.js
@@ -1,10 +1,11 @@
 const ProjectDetail = require("../models/project_detail.js");
 const ProjectPhotos = require("../models/project_photo.js");
+
 const controller = {
   async showProjectDetailPage(req, res) {
-    const projectDetail = await ProjectDetail.findOne({ _id: "62e61267af093b53dde5f5c8" });
-    const projectPhotos = await ProjectPhotos.find({ project_detail_id: "62e61267af093b53dde5f5c8" }, "photo_url");
-    console.log(projectDetail);
+    const projectId = req.params.project_detail_id;
+    const projectDetail = await ProjectDetail.findOne({ _id: projectId });
+    const projectPhotos = await ProjectPhotos.find({ project_detail_id: projectId }, "photo_url");
     res.render("./project-detail/index.ejs", { projectPhotos, projectDetail });
   },
 };
--- routers/project_detail_router.js
+++ routers/project_detail_router.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const projectDetailController = require("../controllers/project_detail.js");
 
-router.get("/", projectDetailController.showProjectDetailPage);
+router.get("/:project_detail_id", projectDetailController.showProjectDetailPage);
 
 module.exports = router;
