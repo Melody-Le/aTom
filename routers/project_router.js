5642b6e695c4cff0fa01f76b440d8675e5c90985
MUI LE <tanmui1992@gmail.com>
2022-08-04 00:13:44 +0800

    feat(project): show form to edit project
    Method: GET

--- routers/project_router.js
+++ routers/project_router.js
@@ -4,7 +4,7 @@ const projectController = require("../controllers/project_controller.js");
 
 router.get("/new", projectController.newProjectForm);
 router.post("/new", projectController.create);
-router.get("/:project_id", projectController.showProjectPage);
+router.get("/:project_id/edit", projectController.editProject);
 router.get("/:project_id", projectController.showProjectPage);
 
 module.exports = router;
