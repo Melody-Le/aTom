fbd278b85bb5331150c3e218ed4bb404c6f1f265
MUI LE <tanmui1992@gmail.com>
2022-08-06 22:24:55 +0800

    feat(navbar): update for idea-market and freelancer community page

--- controllers/page_controller.js
+++ controllers/page_controller.js
@@ -3,16 +3,18 @@ const Projects = require("../models/projects.js");
 const controller = {
   async showFreelancerCommunityPage(req, res) {
     try {
+      const authenticatedUser = req.session.currentUser;
       const userList = await Users.find({});
-      res.render("./pages/freelancer_community.ejs", { userList });
+      res.render("./pages/freelancer_community.ejs", { userList, authenticatedUser });
     } catch (error) {
       console.log(error.message);
     }
   },
   async showIdeaMarketPage(req, res) {
     try {
+      const authenticatedUser = req.session.currentUser;
       const projects = await Projects.find({}, { photos: 1 });
-      res.render("./pages/idea_market.ejs", { projects });
+      res.render("./pages/idea_market.ejs", { projects, authenticatedUser });
     } catch (error) {
       console.log(error.message);
     }
