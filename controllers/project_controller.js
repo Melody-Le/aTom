79837356a1963d953290a8696cc14722c377d9c6
MUI LE <tanmui1992@gmail.com>
2022-08-06 13:23:01 +0800

    feat(project): done delete photo
    Bug: when add photo, the old photo was disappear.

--- controllers/project_controller.js
+++ controllers/project_controller.js
@@ -35,7 +35,8 @@ const controller = {
   //Method PUT: to update project of specific ID
   async updateProject(req, res) {
     const projectId = req.params.project_id;
-    Projects.findByIdAndUpdate(projectId, req.body, { new: true }, (err, product) => {
+    const projectUpdate = req.body;
+    Projects.findByIdAndUpdate(projectId, projectUpdate, { new: true }, (err, product) => {
       if (err) {
         console.log(err);
       }
@@ -44,7 +45,11 @@ const controller = {
   },
   //Method DELETE:  delete photo
   async deletePhoto(req, res) {
-    res.send("delete");
+    const { project_id, photoIndex } = req.params;
+    const project = await Projects.findById(project_id);
+    project.photos.splice(photoIndex, 1);
+    await project.save();
+    res.redirect(`/projects/${project_id}/edit`);
   },
 };
 module.exports = controller;
