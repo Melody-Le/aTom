c490aeb11a281c6fce6df9063d97343488c6969e
MUI LE <tanmui1992@gmail.com>
2022-08-08 00:51:01 +0800

    feat(auth_middleware): update auth-middleware into job-router & project touter

--- routers/job_router.js
+++ routers/job_router.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const jobController = require("../controllers/job_controller.js");
+const { authenticatedOnly } = require("../middlewares/auth_middleware");
 
-router.get("/new", jobController.new);
-router.post("/new", jobController.create);
-router.delete("/:job_id/:skillIndex/delete-skill", jobController.deleteSkill);
-router.delete("/:job_id/:photoIndex/delete-photo", jobController.deletePhoto);
-router.put("/:job_id", jobController.update);
-router.get("/:job_id/edit", jobController.edit);
-router.delete("/:job_id/delete", jobController.deletejob);
-router.get("/:job_id", jobController.showJobPage);
+router.get("/new", authenticatedOnly, jobController.new);
+router.post("/new", authenticatedOnly, jobController.create);
+router.delete("/:job_id/:skillIndex/delete-skill", authenticatedOnly, jobController.deleteSkill);
+router.delete("/:job_id/:photoIndex/delete-photo", authenticatedOnly, jobController.deletePhoto);
+router.put("/:job_id", authenticatedOnly, jobController.update);
+router.get("/:job_id/edit", authenticatedOnly, jobController.edit);
+router.delete("/:job_id/delete", authenticatedOnly, jobController.deletejob);
+router.get("/:job_id", authenticatedOnly, jobController.showJobPage);
 
 module.exports = router;
